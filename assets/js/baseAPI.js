1484e1ffb8c8ca38a963e55ebced0b2abf40d933
zhangyufan <281671126@qq.com>
2021-07-23 13:42:01 +0800

    tijiao

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,11 +4,7 @@
 $.ajaxPrefilter(function(options) {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
   // options.url = 'http://ajax.frontend.itheima.net' + options.url
-<<<<<<< HEAD
-  options.url = 'http://api-breakingnews-web.itheima.net' + options.url
-=======
   options.url = 'http://api-breakingnews-web.itheima.net/' + options.url
->>>>>>> login
 
   // 统一为有权限的接口，设置 headers 请求头
   if (options.url.indexOf('/my/') !== -1) {
