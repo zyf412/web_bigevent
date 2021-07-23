538a9b70af5f0a4014a703b9650c317dfa208be0
zhangyufan <281671126@qq.com>
2021-07-23 15:32:09 +0800

    tijiao

--- assets/js/login.js
+++ assets/js/login.js
@@ -40,7 +40,7 @@ $(function() {
       username: $('#form_reg [name=username]').val(),
       password: $('#form_reg [name=password]').val()
     }
-    $.post('api/reguser', data, function(res) {
+    $.post('/api/reguser', data, function(res) {
       if (res.status !== 0) {
         return layer.msg(res.message)
       }
@@ -55,7 +55,7 @@ $(function() {
     // 阻止默认提交行为
     e.preventDefault()
     $.ajax({
-      url: 'api/login',
+      url: '/api/login',
       method: 'POST',
       // 快速获取表单中的数据
       data: $(this).serialize(),
