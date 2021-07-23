62843115be1dd9b230469a98e184c057a18a0eed
zhangyufan <281671126@qq.com>
2021-07-23 14:19:08 +0800

    tijiao

--- assets/js/login.js
+++ assets/js/login.js
@@ -40,7 +40,7 @@ $(function() {
       username: $('#form_reg [name=username]').val(),
       password: $('#form_reg [name=password]').val()
     }
-    $.post('api/reguser', data, function(res) {
+    $.post('/api/reguser', data, function(res) {
       if (res.status !== 0) {
         return layer.msg(res.message)
       }
